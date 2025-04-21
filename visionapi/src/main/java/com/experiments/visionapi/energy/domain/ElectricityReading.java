54b456df7c3138641e5dd2108ef2a6795b7c173e
newcoder310 <61921140+newcoder310@users.noreply.github.com>
2025-04-21 19:57:36 +0200

    Delete visionapi/src/main/java/com/experiments/visionapi/energy directory

--- visionapi/src/main/java/com/experiments/visionapi/energy/domain/ElectricityReading.java
+++
@@ -1,9 +0,0 @@
-package uk.tw.energy.domain;
-
-import java.math.BigDecimal;
-import java.time.Instant;
-
-/**
- * @param reading kW
- */
-public record ElectricityReading(Instant time, BigDecimal reading) {}
--- visionapi/src/main/java/com/experiments/visionapi/energy/domain/MeterReadings.java
+++
@@ -1,5 +0,0 @@
-package uk.tw.energy.domain;
-
-import java.util.List;
-
-public record MeterReadings(String smartMeterId, List<ElectricityReading> electricityReadings) {}
